a9074b25769eba72e184fcca2d79c0f23499dbc9
CysionLiu <liuxshang@163.com>
2019-09-05 15:11:42 +0800

    add global config;

--- wedialog/src/main/java/com/cysion/wedialog/WeDialog.kt
+++ wedialog/src/main/java/com/cysion/wedialog/WeDialog.kt
@@ -1,53 +1,79 @@
 package com.cysion.wedialog
 
+import android.view.Gravity
 import androidx.fragment.app.FragmentActivity
 import com.cysion.wedialog.dialogs.CustomDialog
 import com.cysion.wedialog.dialogs.LoadingDialog
 
-class WeDialog {
+/*
+* Manager for all real dialogs' invocation*/
 
+object WeDialog {
 
+    //global loading
+    private var loadingDialog: LoadingDialog? = null
+    //global config,can init once outside
+    internal var weConfig = Config()
 
+    private var hasInit = false
 
+    fun initOnce(config: Config) {
+        if (hasInit) {
+            return
+        }
+        hasInit = true
+        weConfig = config
+    }
 
     /*
-    全局loading框
-     */
-    companion object {
-        private var loadingDialog: LoadingDialog? = null
-
-
-
-        fun loading(activity: FragmentActivity) {
-            //在加载中，则不操作
-            loadingDialog?.run {
-                dialog?.run {
-                    if (isShowing) {
-                        return
-                    }
+    * begin loading*/
+    fun loading(activity: FragmentActivity) {
+        //do nothing when loading
+        loadingDialog?.run {
+            dialog?.run {
+                if (isShowing) {
+                    return
                 }
             }
-            if (activity == null || activity.isFinishing) {
-                return
-            }
-            loadingDialog = LoadingDialog()
-            loadingDialog?.show(activity.supportFragmentManager, "")
-
         }
+        if (activity.isFinishing) {
+            return
+        }
+        loadingDialog = LoadingDialog()
+        loadingDialog?.show(activity.supportFragmentManager, "")
 
-        fun dismiss() {
-            loadingDialog?.run {
-                dialog?.run {
-                    if (isShowing) {
-                        loadingDialog?.dismissAllowingStateLoss()
-                    }
+    }
+
+    /*
+    * dismiss loading dialog*/
+    fun dismiss() {
+        loadingDialog?.run {
+            dialog?.run {
+                if (isShowing) {
+                    loadingDialog?.dismissAllowingStateLoss()
                 }
-                loadingDialog = null
             }
+            loadingDialog = null
         }
+    }
 
-         fun custom(activity: FragmentActivity):CustomDialog.Builder{
-            return CustomDialog.Builder(activity)
-        }
+    fun custom(activity: FragmentActivity): CustomDialog.Builder {
+        return CustomDialog.Builder(activity)
+    }
+
+
+
+    class Config {
+        //0-1f
+        var mDimCount = 0.5f
+
+        var mGravity = Gravity.CENTER
+
+        //0-1f
+        var mWidthRatio = 0.9f
+
+        var mCancelable = true
+
+        var mCancelableOutSide = false
     }
 }
--- wedialog/src/main/java/com/cysion/wedialog/dialogs/LoadingDialog.kt
+++ wedialog/src/main/java/com/cysion/wedialog/dialogs/LoadingDialog.kt
@@ -4,12 +4,12 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentManager
 import com.cysion.wedialog.R
+import com.cysion.wedialog.WeDialog
 
 class LoadingDialog : DialogFragment() {
-
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         activity?.run {
@@ -18,23 +18,21 @@ class LoadingDialog : DialogFragment() {
             val view = inflater.inflate(R.layout.we_dialog_loading, null)
             val dialog = builder.create()
             val window = dialog.window
-            dialog.setCanceledOnTouchOutside(false)
-            dialog.setCancelable(true)
+            dialog.setCanceledOnTouchOutside(WeDialog.weConfig.mCancelableOutSide)
+            setCancelable(WeDialog.weConfig.mCancelable)
             //摆脱token的限制，注意清单文件alert权限
             val p = window!!.attributes // 获取对话框当前的参数值
             window.decorView.setBackgroundColor(0X00000000)
-            p.width = (resources.displayMetrics.widthPixels).toInt()
+            p.width = (resources.displayMetrics.widthPixels*WeDialog.weConfig.mWidthRatio).toInt()
             window.attributes = p
-            window.setDimAmount(0.3f)
+            window.setDimAmount(WeDialog.weConfig.mDimCount)
             window.setBackgroundDrawable(null)
             dialog.show()
             dialog.setContentView(view)
+            dialog.window!!.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM)
+            dialog.window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
             return dialog
         }
         return super.onCreateDialog(savedInstanceState)
-    }
-
-    override fun show(manager: FragmentManager?, tag: String?) {
-        super.show(manager, tag)
     }
 }
